32bbb406c4d1a9a5752f12323fe339ebce34e0d8
Akshay Rajguru <akshaygururaj141@gmail.com>
2021-10-31 12:59:52 +0530

    added LimitOffsetPagination

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,9 +1,15 @@
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 
 from store.serializers import ProductSerializer
 from store.models import Product
+
+
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 
 class ProductList(ListAPIView):
@@ -17,6 +23,11 @@ class ProductList(ListAPIView):
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
     search_fields = ('name', 'description')
+    # http://127.0.0.1:8000/api/v1/products/?limit=1&offset=1
+    # This is because we have specified the limit as 3 which indicates that the
+    # page size is 1 and an offset of 1 means that the starting point of this page
+    # should be after the first 1 rows in the result-set.
+    pagination_class = ProductsPagination
 
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
